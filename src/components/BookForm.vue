a1fb694677e96a380b267c8187ce46f91e9aa31a
Eamonn <eamonne@gmail.com>
2020-04-12 11:50:29 -0400

    new

---
+++ src/components/BookForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'BookForm',
+  props: ['books'],
+  data: function() {
+    let bookTitle = '';
+    let bookAuthor = '';
+    return bookTitle, bookAuthor
+  }
+}
+</script>
+<style lang="css">
+  
+</style>
